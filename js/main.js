c44a9ee82ed602cd59578852a73e50d558ed66c1
Qnweii <qnwei.wong@gmail.com>
2025-08-05 03:36:56 +0800

    refractor home page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+document.addEventListener("keydown", function(event){
+	if (event.ctrlKey){
+		event.preventDefault();
+	}
+
+	if (event.keyCode == 123) {
+		event.preventDefault();
+	}
+})
+
+document.addEventListener('contextmenu', 
+	event => event.preventDefault()
+);
+
 ;(function () {
 	
 	'use strict';
